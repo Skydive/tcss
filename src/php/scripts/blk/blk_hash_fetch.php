8227e99d287329eb787e1bfb99b9c4928e8f99bf
Khalid Aleem <me@khalidaleem.com>
2018-12-11 17:43:28 +0000

    memes

---
+++ src/php/scripts/blk/blk_hash_fetch.php
@@ -0,0 +1,34 @@
+<?php
+require_once("lib/core/database.php");
+require_once("lib/core/exception.php");
+require_once("lib/blk/blk.php");
+
+
+$blk_id = (int)$inputs['blk_id'];
+
+try {
+	SKYException::CheckNULL($blk_id, "blk", "blk_id_unspecified");
+	
+	$db = Database::Connect($GLOBALS['project_name']);
+	
+	$blk_row = Content_Blk::Query([
+		'db' => $db,
+		'blk_id' => $blk_id,
+		'request' => ['blk_id','blk_hash']
+	]);
+	if(!$blk_row) {
+		SKYException::Send([
+			'type' => 'blk',
+			'error' => 'id_missing'
+		]);
+	}
+
+	Output::SetNotify('status', 'success');
+	Output::SetNotify('blk_id', $blk_row['blk_id']);
+	Output::SetNotify('blk_hash', $blk_row['blk_hash']);
+	
+} catch (SKYException $e) {
+	if($db) $db->rollback();
+	SKYException::Notify();
+}
+?>
